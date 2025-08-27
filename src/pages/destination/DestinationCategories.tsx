8c29b9dce58404355e6be594eb0e938c2f5c5b94
Onyekachi Jasper-Duruzor <ojasperduruzor@yahoo.com>
2025-08-27 23:21:11 +0100

    Desktop Destination styling

--- src/pages/destination/DestinationCategories.tsx
+++ src/pages/destination/DestinationCategories.tsx
@@ -5,7 +5,7 @@ const DestinationCategories = ({
   activeIndex,
 }) => {
   return (
-    <div className="absolute top-120 left-1/2 transform -translate-x-1/2 flex gap-10 text-white">
+    <div className="absolute top-120 left-1/2 transform -translate-x-1/2 flex gap-10 text-white lg:top-65 lg:right-89 lg:left-auto lg:translate-x-0">
       {/* <div className="text-white flex gap-10 absolute top-110 left-10 md:text-center"> */}
       {categories.map((category, index) => {
         const isActive = index === activeIndex;
@@ -18,7 +18,7 @@ const DestinationCategories = ({
               filterItems(category);
               handleIsActive(index);
             }}
-            className={`font-barlow-condensed uppercase tracking-widest pb-2 transition-colors duration-300 ${
+            className={`font-barlow-condensed uppercase tracking-widest pb-2 transition-colors duration-300  ${
               isActive
                 ? "text-white border-b-2 border-white"
                 : "text-[var(--primary-color)]  border-b-2 border-transparent hover:border-[var(--primary-color)] hover:text-white"
--- src/pages/destination/DestinationMenuItem.tsx
+++ src/pages/destination/DestinationMenuItem.tsx
@@ -1,34 +1,69 @@
 const DestinationMenuItem = ({ id, title, info, distance, time, image }) => {
   return (
-    <div
-      className="lg:hidden text-white mt-15 flex flex-col justify-center items-center gap-10"
-      key={id}
-    >
-      <img
-        src={image}
-        alt="Image of the Moon"
-        className="w-[228.54px] h-[228.54px]"
-      />
-      <p className="font-bellefair text-7xl md:text-8xl mt-20">{title}</p>
-      <p className="font-barlow-regular tracking-wide text-center sm:text-wrap sm:w-[579px] text-[var(--primary-color)] px-4">
-        {info}
-      </p>
-      <hr className="w-85 border-t-2 border-white/25 md:w-130" />
-      <div className="md:flex gap-50">
-        <div className="text-center">
-          <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
-            AVG. DISTANCE
-          </p>
-          <p className="text-4xl mt-3 font-bellefair">{distance}</p>
-        </div>
-        <div className="text-center mb-5">
-          <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
-            EST. TRAVEL TIME
-          </p>
-          <p className="inline-block text-4xl mt-3 font-bellefair">{time}</p>
+    <>
+      <div
+        className="lg:hidden text-white mt-15 flex flex-col justify-center items-center gap-10"
+        key={id}
+      >
+        <img
+          src={image}
+          alt="Image of the Moon"
+          className="w-[228.54px] h-[228.54px]"
+        />
+        <p className="font-bellefair text-7xl md:text-8xl mt-20">{title}</p>
+        <p className="font-barlow-regular tracking-wide text-center sm:text-wrap sm:w-[579px] text-[var(--primary-color)] px-4">
+          {info}
+        </p>
+        <hr className="w-85 border-t-2 border-white/25 md:w-130" />
+        <div className="md:flex gap-50">
+          <div className="text-center">
+            <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
+              AVG. DISTANCE
+            </p>
+            <p className="text-4xl mt-3 font-bellefair mb-5">{distance}</p>
+          </div>
+          <div className="text-center mb-5">
+            <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
+              EST. TRAVEL TIME
+            </p>
+            <p className="inline-block text-4xl mt-3 font-bellefair">{time}</p>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className="hidden lg:flex justify-center items-end h-full text-white gap-50 mt-[100px]"
+        key={id}
+      >
+        <img
+          src={image}
+          alt="Image of the Moon"
+          className="w-[450px] h-[450px]"
+        />
+        <div>
+          <p className="font-bellefair text-7xl text-8xl mt-20 ">{title}</p>
+          <p className="font-barlow-regular tracking-wide text-left pt-7  pb-9 text-wrap w-[405px] text-[var(--primary-color)]">
+            {info}
+          </p>
+          <hr className="w-105 border-t-1 border-white/25 " />
+          <div className="flex gap-30 pt-10">
+            <div className="text-left">
+              <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
+                AVG. DISTANCE
+              </p>
+              <p className="text-2xl mt-3 font-bellefair">{distance}</p>
+            </div>
+            <div className="text-left mb-5">
+              <p className="text-[var(--primary-color)] font-barlow-condensed tracking-widest">
+                EST. TRAVEL TIME
+              </p>
+              <p className="inline-block text-2xl mt-3 font-bellefair">
+                {time}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default DestinationMenuItem;
