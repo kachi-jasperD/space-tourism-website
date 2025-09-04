5c87404356b5ca32de8aa22e3b51d38e59df8166
Onyekachi Jasper-Duruzor <ojasperduruzor@yahoo.com>
2025-09-04 16:57:56 +0100

    sidebar Menu

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import {
   Outlet,
   createRootRoute,
@@ -7,6 +8,7 @@ import {
 import NavBar from "@/pages/NavBar";
 import menu from "/assets/shared/icon-hamburger.svg";
 import Home from "@/pages/Home";
+import close from "/assets/shared/icon-close.svg";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -18,6 +20,8 @@ function RootComponent() {
     select: (state) => state.location,
   });
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <React.Fragment>
       {/* Only show Home when at "/" */}
@@ -27,7 +31,48 @@ function RootComponent() {
       <NavBar />
 
       {/* This is the menu icons */}
-      <img src={menu} alt="" className="absolute top-5 right-5 md:hidden" />
+      <img
+        src={menu}
+        alt=""
+        className="absolute top-5 right-5 md:hidden"
+        onClick={() => setIsOpen(true)}
+      />
+
+      {/* Side Menu Drawer */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-black/60 z-40 md:hidden">
+          <div className="absolute top-0 right-0 h-full w-2/3 bg-[#0B0D17] p-6 flex flex-col text-white">
+            {/* Close button */}
+            <button className="self-end mb-10" onClick={() => setIsOpen(false)}>
+              <img src={close} alt="Close menu" />
+            </button>
+
+            {/* Nav Links */}
+            <a href="/" className="mb-6" onClick={() => setIsOpen(false)}>
+              <span className="mr-2 font-barlow-condensed-bold">00</span> HOME
+            </a>
+            <a
+              href="/destination"
+              className="mb-6"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="mr-2 font-barlow-condensed-bold">01</span>{" "}
+              DESTINATION
+            </a>
+            <a href="/crew" className="mb-6" onClick={() => setIsOpen(false)}>
+              <span className="mr-2 font-barlow-condensed-bold">02</span> CREW
+            </a>
+            <a
+              href="/technology"
+              className="mb-6"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="mr-2 font-barlow-condensed-bold">03</span>{" "}
+              TECHNOLOGY
+            </a>
+          </div>
+        </div>
+      )}
 
       <Outlet />
     </React.Fragment>
